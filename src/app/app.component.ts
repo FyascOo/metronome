953a7647ce2e5b90a3e0018eade81c3f7f78e5f9
achoufa <alan.choufa@gmail.com>
2023-07-03 21:56:59 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'metronome-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `Hello`,
 })
 export class AppComponent {
   title = 'metronome';
