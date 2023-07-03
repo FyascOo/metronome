48cfb2f9079ebe2fc5355673c5df5a9ed1dfd8e0
achoufa <alan.choufa@gmail.com>
2023-07-03 23:11:10 +0200

    wip: test import material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   imports: [RouterModule, MatButtonModule],
   selector: 'metronome-root',
-  template: `<button mat-button>Basic</button>`,
+  template: `<button mat-raised-button color="primary">Basic</button>`,
 })
 export class AppComponent {
   title = 'metronome';
